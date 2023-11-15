5d4c809b7d469beb0e13507a07c9e9059f8e3749
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-11-15 15:38:25 +1100

    engine

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod matching_engine;
+use matching_engine::engine::{MatchingEngine, TradingPair};
 use matching_engine::orderbook::{BidOrAsk, Order, Orderbook};
 
 fn main() {
@@ -12,5 +13,8 @@ fn main() {
     let sell_order = Order::new(BidOrAsk::Ask, 6.5);
     orderbook.add_order(20.0, sell_order);
 
-    println!("{:?}", orderbook);
+    //println!("{:?}", orderbook);
+    let mut engine = MatchingEngine::new();
+    let pair = TradingPair::new("BTC".to_string(), "USD".to_string());
+    engine.add_new_market(pair);
 }
